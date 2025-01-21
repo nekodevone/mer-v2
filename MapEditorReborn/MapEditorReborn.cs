1cb8d4a361a5b2d2819f4a0546298855beb541d6
JasKill <jaskll000@gmail.com>
2025-01-21 21:21:19 +0700

    chore: bump 3.2.0

--- MapEditorReborn/MapEditorReborn.cs
+++ MapEditorReborn/MapEditorReborn.cs
@@ -215,7 +215,7 @@ namespace MapEditorReborn
         public override string Author => "Michal78900, NekoDev Team";
 
         /// <inheritdoc/>
-        public override Version Version { get; } = new (3, 1, 9);
+        public override Version Version { get; } = new (3, 2, 0);
 
         /// <inheritdoc/>
         public override Version RequiredExiledVersion { get; } = new (9, 0, 0);
